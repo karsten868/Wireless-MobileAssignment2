f3f99e5886b65a7db214d20e992c122f8dcbc0f3
KARSTEN LANGE <karstenlange1999@gmail.com>
2021-11-16 08:55:39 -0400

    second commit

--- app/src/main/java/com/example/productmnagementapp/TopLevelActivity.java
+++ app/src/main/java/com/example/productmnagementapp/TopLevelActivity.java
@@ -3,12 +3,25 @@ package com.example.productmnagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TopLevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_toplevel);
     }
+
+
+    public void onClickReceive(View view){
+
+    }
+
+    public void onClickOrder(View view) {
+
+    }
+
+
+
 }
